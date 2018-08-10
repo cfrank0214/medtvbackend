b30a70166376c536e7dbe8633bfdac988c588e28
JuliaKan <39783445+JuliaKan@users.noreply.github.com>
2018-08-10 14:25:48 -0400

    made list-test.js

---
+++ videos/list-test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fakeData = [
+    {
+        "patient": {
+            "gender": "female",
+            "age": "28"
+        },
+        "vid_location": {
+            "title": "UVM Medical",
+            "country": "USA"
+        },
+        "device": "Medtronic MRI SureScan",
+        "updatedAt": 1533922211025,
+        "vid_uri": "www.youtube.com",
+        "createdAt": 1533917967841,
+        "description": "VARISES is helping create better-trained surgeons and surgical staff to meet the rapidly growing demand for procedures such as joint replacement surgeries.",
+        "id": "27620c20-9cb9-11e8-b2ad-d524ebffd498",
+        "tags": [
+            "Hip",
+            "Training",
+            "VR",
+            "PaceMaker"
+        ],
+        "author": "Dr. Mat Gilbert",
+        "title": "Hip Surgery 1.5",
+        "vid_duration": "2735"
+    }
+]
+
+module.exports.list = (event, context, callback) => {
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(fakeData),
+    };
+    callback(null, response);
+};
+
