bd5c5a601a9187a29da7b50ff70efc3a22089b39
ipointer2 <chris.e.frank@gmail.com>
2018-08-17 11:52:19 -0400

    now getting stringParameters

--- videos/pagination.js
+++ videos/pagination.js
@@ -5,11 +5,28 @@ const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-depe
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.pagination = (event, context, callback) => {
+	if (event.queryStringParameters !== null && event.queryStringParameters !== undefined) {
+		if (
+			event.queryStringParameters.limit !== undefined &&
+			event.queryStringParameters.limit !== null &&
+			event.queryStringParameters.limit !== ''
+		) {
+			console.log('Received limit: ' + event.queryStringParameters.limit);
+			let limit = event.queryStringParameters.limit;
+		} if (
+			event.queryStringParameters.startkey !== undefined &&
+			event.queryStringParameters.startkey !== null &&
+			event.queryStringParameters.startkey !== ''
+		) {
+			console.log('Received startkey: ' + event.queryStringParameters.startkey);
+			let startkey = event.queryStringParameters.startkey;
+		}
+	}
 	let params = {
 		TableName: process.env.DYNAMODB_TABLE,
 		Limit: 5,
 		ExclusiveStartKey: {
-			"id": "dfea80c0-9fd8-11e8-920a-a77773504cc3"
+			id: 'dfea80c0-9fd8-11e8-920a-a77773504cc3'
 		}
 	};
 	// fetch all video from the database
